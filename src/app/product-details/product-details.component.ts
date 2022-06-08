8aac3aac81d14d2c18a6759bb13f813bb9716aa9
Regentanz <butterbeamer@gmail.com>
2022-06-08 22:04:17 +0200

    Adding navigation

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -7,8 +7,15 @@ import { Product, products } from '../products';
   styleUrls: ['./product-details.component.css'],
 })
 export class ProductDetailsComponent implements OnInit {
-  product!: Product;
+  product: Product | undefined;
   constructor(private route: ActivatedRoute) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+
+    this.product = products.find(
+      (product) => product.id === productIdFromRoute
+    );
+  }
 }
